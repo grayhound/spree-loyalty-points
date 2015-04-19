7480ad9bd122a1c78f4372a0b7f37ad706d81c5a
hrom <roman@alltmb.ru>
2015-04-19 14:51:26 +0300

    change settings template

--- app/controllers/spree/admin/general_settings_controller_decorator.rb
+++ app/controllers/spree/admin/general_settings_controller_decorator.rb
@@ -5,13 +5,13 @@ Spree::Admin::GeneralSettingsController.class_eval do
   private
 
     def set_loyalty_points_settings
-      @preferences_loyalty_points = {
-        :min_amount_required_to_get_loyalty_points => [""],
-        :loyalty_points_awarding_unit => [Spree.t(:loyalty_points_awarding_unit_description)],
-        :loyalty_points_redeeming_balance => [""],
-        :loyalty_points_conversion_rate => [Spree.t(:loyalty_points_conversion_rate_description)],
-        :loyalty_points_award_period => [""]
-      }
+      @preferences_loyalty_points = [
+        :min_amount_required_to_get_loyalty_points,
+        :loyalty_points_awarding_unit,
+        :loyalty_points_redeeming_balance,
+        :loyalty_points_conversion_rate,
+        :loyalty_points_award_period
+      ]
     end
 
 end
--- app/overrides/add_loyalty_points_preferences_to_general_settings_page.rb
+++ app/overrides/add_loyalty_points_preferences_to_general_settings_page.rb
@@ -1,18 +1,19 @@
 Deface::Override.new(:virtual_path => 'spree/admin/general_settings/edit',
   :name => 'add_loyalty_points_preferences_to_general_settings_page',
-  :insert_before => "div.row",
+  :insert_before => ".form-actions[data-hook='buttons']",
   :text => "
-    <div class='row'>
-      <fieldset class='loyalty-points no-border-bottom'>
-        <legend align='center'><%= Spree.t(:loyalty_points_settings)%></legend>
-        <% @preferences_loyalty_points.each do |key, value|
-            type = Spree::Config.preference_type(key) %>
-            <div class='field'>
-              <%= label_tag(key, Spree.t(key)) + tag(:br) if type != :boolean %>
-              <%= preference_field_tag(key, Spree::Config[key], :type => type) %>
-              <p><%= value[0] %></p>
-            </div>
+    <div class='panel panel-default'>
+      <div class='panel-heading'>
+        <h1 class='panel-title'><%= Spree.t(:loyalty_points_settings) %></h1>
+      </div>
+      <div class='panel-body'>
+        <% @preferences_loyalty_points.each do |key| %>
+          <% type = Spree::Config.preference_type(key) %>
+          <div class='form-group'>
+            <%= label_tag key, Spree.t(key) %>
+            <%= preference_field_tag(key, Spree::Config[key], type: type) %>
+          </div>
         <% end %>
-      </fieldset>
+      </div>
     </div>
   ")
